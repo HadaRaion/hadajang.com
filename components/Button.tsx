778becb2d9c672cef6c09b8f1a169a92368ea4e6
hada <sweet.raion@gmail.com>
2023-06-27 11:10:59 -0700

    Fix the problem with scroll down

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,13 +10,9 @@ type ButtonProps = {
 export default function Button({ href, label }: ButtonProps) {
 	const setSpanPosition = (e: React.MouseEvent<HTMLAnchorElement>) => {
 		if (e.currentTarget) {
-			console.log(e.currentTarget);
-
-			console.log(e.currentTarget.getBoundingClientRect());
-			console.log(e.pageX);
 			let parentOffset = e.currentTarget.getBoundingClientRect(),
 				relX = e.pageX - parentOffset.left,
-				relY = e.pageY - parentOffset.top;
+				relY = e.pageY - parentOffset.top - window.scrollY; // Subtract the current scroll position
 			e.currentTarget.querySelector('span')!.style.top = relY + 'px';
 			e.currentTarget.querySelector('span')!.style.left = relX + 'px';
 		}
@@ -32,7 +28,7 @@ export default function Button({ href, label }: ButtonProps) {
 
 	return (
 		<a
-			className="btn relative flex items-center justify-center w-56 h-20 border-[3px] border-black rounded-[1.2rem] overflow-hidden font-round text-xl font-bold"
+			className="btn relative flex items-center justify-center w-56 h-20 border-hada font-round text-xl font-bold outline-"
 			href={href}
 			target="_blank"
 			title="New tap"
