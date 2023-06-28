62a688a356f6c3e4f8f208a003fc9f6eb9b37e88
hada <sweet.raion@gmail.com>
2023-06-28 15:58:53 -0700

    Change meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ const nunito_sans = Nunito_Sans({
 });
 
 export const metadata = {
-	title: 'Hada Jang',
+	metadataBase: new URL('https://www.hadajang.com/'),
+	title: 'Front End Developer - Hada Jang',
 	description:
 		'A creative Front end developer blending design expertise with technical skills to deliver compelling web experiences.',
 	applicationName: 'Hada Jang',
