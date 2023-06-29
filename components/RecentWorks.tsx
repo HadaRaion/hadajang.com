12d1fbe7a8b6f102fbb2d7bbd86fbbfb360db45e
hada <sweet.raion@gmail.com>
2023-06-28 22:20:47 -0700

    Shorten descriptions for latest works

--- components/RecentWorks.tsx
+++ components/RecentWorks.tsx
@@ -6,7 +6,7 @@ export default function RecentWorks() {
 		{
 			title: 'Amanda de Waal Therapy',
 			description:
-				'A project created for a newly registered clinical counselor, Amanda de Waal Therapy involved designing a logo to match the desired aesthetic. With a focus on SEO optimization and fast TTV and FCP, the website was developed using Next.js. To facilitate easy contact, Nodemailer was integrated to enable visitors to send emails directly from the website. Instagram API was utilized to display client feeds, establishing a sense of familiarity and trust. Additionally, a prominent link for additional appointments was placed at the bottom of the screen. The website was registered on Google and attracted 4 new patients within the first month.',
+				'Created a comprehensive website for a Registered Clinical Counsellor, including logo design, web design, and development.  The website garnered 4 new patients in the first month after launch. Leveraged Next.js for fast loading times and optimized SEO. Implemented Nodemailer for seamless email communication and integrated Instagram API to display client feeds. Added a convenient appointment booking link for existing patients.',
 			skills: [
 				'React',
 				'Next.js',
@@ -21,7 +21,7 @@ export default function RecentWorks() {
 		{
 			title: 'Aestehclear',
 			description:
-				"Developed the website for Aestheclear, a Sydney-based skincare clinic. Despite budget constraints, we achieved robust internal content management through a customized WordPress theme. Carefully selected a theme aligned with the clinic's brand, making adjustments to colors, fonts, and layout. Optimized search performance using Yoast SEO plugin. The result is a professional, user-friendly website showcasing Aestheclear's services within budgetary constraints.",
+				"Developed the website for Aestheclear, a Sydney-based skincare clinic. Despite budget constraints, achieved robust internal content management through a customized WordPress theme. Carefully selected a theme aligned with the clinic's brand, making adjustments to colors, fonts, and layout. Optimized search performance using Yoast SEO plugin. The result is a professional, user-friendly website showcasing Aestheclear's services within budgetary constraints.",
 			skills: ['PHP', 'CSS', 'JavaScript', 'WordPress'],
 			demoLink: 'https://aestheclear.com/',
 			image: '/images/thumb-aestheclear.jpg',
@@ -29,7 +29,7 @@ export default function RecentWorks() {
 		{
 			title: 'KEAD Webzine',
 			description:
-				'This project involved transforming a monthly magazine published by the Korea Employment Agency for Persons with Disabilities into a web-based magazine. WordPress CMS was used for internal article uploads, and a custom theme was developed for a tailored design. A WordPress admin page block editor was created to allow unique designs for articles in different categories. A form page was created for subscription reviews and event participation. Integration with the Stibee newsletter service resulted in over 600 subscribers receiving monthly newsletters. The website prioritized web accessibility and received certification from the Ministry of Science and ICT.',
+				'Transformed a monthly print magazine by Korea Employment Agency for Persons with Disabilities into a web-based magazine. Developed a custom WordPress theme for uploading articles and created a user-friendly block editor for different designs. Integrated a Form page for subscriber reviews and event participation. Achieved over 600 subscribers through Stibee newsletter service, complying with web accessibility standards and receiving certification.',
 			skills: ['PHP', 'Sass', 'JavaScript', 'WordPress'],
 			demoLink: 'https://kead-webzine.or.kr/',
 			githubLink: 'https://github.com/HadaRaion/kead-webzine',
@@ -38,7 +38,7 @@ export default function RecentWorks() {
 		{
 			title: 'Disaster Alert Mock-up',
 			description:
-				"As part of the national project 'Customized Disaster Media Service Platform Development for Recognizing Vulnerable Groups', a mock-up of a disaster information app was developed for exhibition purposes on UHD TVs and smart devices since 2019. Various scenarios were created for earthquakes, wildfires, and typhoons, and web app planning was conducted. An administrator mode was implemented to send different alerts to devices based on the scenarios. Leveraging the Notification API, push notifications were sent to smart devices, directing users to specific pages for additional information. The demonstration was successfully completed, and the actual app development for service will begin in the latter half of 2023.",
+				"As part of the national project 'Customized Disaster Media Service Platform Development for Recognizing Vulnerable Groups', collaborated with Korean Broadcasting System on a mock-up project for a tailored disaster media service platform. Developed web applications for UHD TVs and smart devices, simulating scenarios such as earthquakes, wildfires, and typhoons. Designed a web app interface and utilized the Notification API to send push alerts and provide additional information. ",
 			skills: ['HTML', 'Sass', 'JavaScript'],
 			githubLink: 'https://github.com/HadaRaion/mobile-disaster-alert',
 			image: '/images/thumb-kbs.jpg',
@@ -46,7 +46,7 @@ export default function RecentWorks() {
 		{
 			title: 'Quantinum Investment',
 			description:
-				'This project involved creating a website for an investment advisory company targeting both domestic and international clients. To optimize SEO and ensure fast TTV and FCP, Next.js was used for development, while i18next was implemented for multilingual services. Lottie was used to convert animations created by a BI production company, providing a slideshow effect on the homepage with faster loading than videos. Carefully selected images showcased expertise, and a clean layout was designed to instill trust. Framer Motion was utilized to add subtle animations, creating a fresh and lightweight user experience.',
+				'Created a website for an investment advisory firm targeting both Korean and international clients. Developed with Next.js for optimized SEO, fast loading times, and multi-language support using i18next. Utilized Lottie animations for a dynamic slideshow effect on the main page. Curated professional photos and implemented a clean layout to instill trust. Employed Framer Motion to add subtle animations for a fresh and engaging user experience.',
 			skills: [
 				'React',
 				'Next.js',
