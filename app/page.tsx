79b4d519ad14834447e467c0e3827e322115ad7e
hada <sweet.raion@gmail.com>
2023-06-30 13:37:09 -0700

    Add Google tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+import Script from 'next/script';
+
 import Hero from '@/components/Hero';
 import RecentWorks from '@/components/RecentWorks';
 import Skills from '@/components/Skills';
-import Image from 'next/image';
 
 export default function Home() {
 	return (
 		<>
+			<Script
+				src={`
+          https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}
+        `}
+			/>
+			<Script id="google-analytics">
+				{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.GA_TRACKING_ID}');
+        `}
+			</Script>
 			<Hero />
 			<Skills />
 			<RecentWorks />
